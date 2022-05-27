1428907390ad88e63a567321b727143ed1d8d78d
andrealiz1 <99044030+andrealiz1@users.noreply.github.com>
2022-05-26 17:45:49 -0700

    ACisneros_Unit4LessonB
    ACisneros_Unit4LessonB

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         float magnitude = forwardInput * speed * Time.deltaTime;
-        rbPlayer.AddForce(focalPoint.transform.forward * forwardInput * speed * Time.deltaTime, ForceMode.Impulse);
+        rbPlayer.AddForce(focalPoint.transform.forward * forwardInput * speed * Time.deltaTime, ForceMode.Force);
 
         Debug.Log(magnitude);
         Debug.Log("FI: " + forwardInput);
