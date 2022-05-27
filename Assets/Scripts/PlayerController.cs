628212e9028d9310d7020f60bc513de619a0324d
andrealiz1 <99044030+andrealiz1@users.noreply.github.com>
2022-05-27 00:52:36 -0700

    ACisneros_Unit4LessonC
    ACisneros_Unit4LessonC

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     GameObject focalPoint;
     Renderer rendererPlayer;
     public float speed = 10.0f;
+    public float powerUpSpeed = 10.0f;
+    public GameObject powerUpInd;
+
+
+    bool hasPowerUp = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +26,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         float magnitude = forwardInput * speed * Time.deltaTime;
-        rbPlayer.AddForce(focalPoint.transform.forward * forwardInput * speed * Time.deltaTime, ForceMode.Force);
-
-        Debug.Log(magnitude);
-        Debug.Log("FI: " + forwardInput);
+        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Force);
 
         if (forwardInput > 0)
         {
@@ -34,7 +36,36 @@ public class PlayerController : MonoBehaviour
         {
             rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
         }
+        powerUpInd.transform.position = transform.position;
 
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdown());
+            powerUpInd.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+      
+        if (hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collid with " + collision.gameObject + "with powerup set to; " + hasPowerUp);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+    IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        powerUpInd.SetActive(false);
+    }
 }
